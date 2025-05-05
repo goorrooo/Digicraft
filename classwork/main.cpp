2fa7f28c903cc27698097bb40887577b565944f5
goorrooo <gorrgrigoryann@gmail.com>
2025-05-05 19:03:02 +0400

    C++

---
+++ classwork/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+/*
+int main(){
+	int num = 7;
+	float a = 7.0;
+	double b = 8.11; //diff double  float - size
+	char c = 'c';
+	bool d = true/false; 
+	std::string string = "Gor";
+}
+*/
+
+
+/*
+int main()
+{
+        int num = 7;
+	std::cout << num << std::endl;
+	std::cout << sizeof(int) << std::endl;
+	float a = 7.0;
+	std::cout << a << std::endl;
+	std::cout << sizeof(float) << std::endl;
+	double b = 8.0;
+	std::cout << b << std::endl;
+	std::cout << sizeof(int) << std::endl;
+	char c = 'a';
+	std::cout << c << std::endl;
+	std::cout << sizeof(char) << std::endl;
+	bool d = true; 
+	std::cout << d << std::endl;
+	std::cout << sizeof(bool) << std::endl;
+	std::string name  = "Gor";
+	std::cout << name << std::endl;
+	std::cout << sizeof(std::string) << std::endl;
+	
+	return 0;
+
+}
+*/
+
+int main()
+{
+	long int  a = 7 ;
+	std::cout << a << std::endl;
+	std::cout << sizeof(int) << std::endl;
+	short int b = 7;
+	std::cout << b << std::endl;
+	std::cout << sizeof(int) << std::endl;
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
