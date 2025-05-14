4aae85e083c011a51d78e146c53b17757a0f2b5b
goorrooo <gorrgrigoryann@gmail.com>
2025-05-14 20:54:34 +0400

    enum calculator

--- classwork/cin.cpp
+++ classwork/cin.cpp
@@ -35,7 +35,7 @@ int main(){
 }
 
 */
-
+/*
 struct emlogee {
 	std::string name;
 	int ashvarc;
@@ -68,6 +68,132 @@ int main (){
 
 
 }
+*/
+//
+//int main(){
+//	std::cout << "Input number: ";
+//	int num = 0;
+//	std::cin >> num;
+//	switch (num){
+//	case 1:
+//	std::cout << "Hashvapahutyun" << std::endl;
+//	break;
+//	case 2:
+//	std::cout << "Vacharq" << std::endl;
+//	break;
+//	case 3:
+//	std::cout << "Tnoren" << std::endl;
+//	break;
+//	case 4:
+//	std::cout << "HR" << std::endl;
+//	break;
+//	case 5:
+//	std::cout << "Srah" << std::endl;
+//	break;
+//	case 6:
+//	std::cout << "Araqum" << std::endl;
+//	break;	
+//	case 7:
+//	std::cout << "Operator" << std::endl;
+//	break;
+//	default:
+//	std::cout << "Unknown" << std::endl;
+//	break;
+//
+//
+//}    
+	
+
+
+
+/*
+enum num {
+	HASHVAPAUTYUN,
+	VACHARQ,
+	TNOREN,
+	HR,
+	SRAH,
+	ARAQUM,
+	OPERATOR,
+	UNKNOWN
+};
+
+
+int main(){
+	enum num inp = VACHARQ;
+	switch(inp){
+	
+	case HASHVAPAUTYUN:
+	std::cout << "Hashvapahutyun" << std::endl;
+	break;
+	case VACHARQ:
+	std::cout << "Vacharq" << std::endl;
+	break;
+	case TNOREN:
+	std::cout << "Tnoren" << std::endl;
+	break;
+	case HR:
+	std::cout << "HR" << std::endl;
+	break;
+	case SRAH:
+	std::cout << "Srah" << std::endl;
+	break;
+	case ARAQUM:
+	std::cout << "Araqum" << std::endl;
+	break;
+	case OPERATOR:
+	std::cout << "Operator" << std::endl;
+	break;
+	default: 
+	std::cout << "Unknown" << std::endl;
+	break;
+
+}
+
+
+}
+
+*/
+
+enum math {
+	GUMARUM,
+	HANUM,
+	BAJANUM,
+	BAZMAPATKUM,
+	UNKNOWN
+};
+
+int main(){
+	enum math gorc;
+	std::cout << "Input number:" << std::endl;
+	int num1 = 0;
+	int num2 = 0;
+	std::cin >> num1;
+	std::cin >> num2;
+	std::cout << "Input operator: +, -, *, /  "  << std::endl;
+	enum math gorc = std::string sym;
+	switch(gorc){
+	case GUMARUM:
+	 	std::cout << num1+num2 << std::endl;
+		break;
+	case HANUM: 
+		std::cout << num1-num2 << std::endl;
+		break;
+	case BAJANUM:
+		std::cout << num1/num2 << std::endl;
+		break;
+	case BAZMAPATKUM: 
+		std::cout << num1*num2 << std::endl;
+		break;
+	default:
+		std::cout << "Unknown" << std::endl;		
+		break;
+}
+	
+
+
+
+}
 
 
 
@@ -76,3 +202,17 @@ int main (){
 
 
 
+
+
+
+
+
+
+
+
+
+	
+
+
+
+	
