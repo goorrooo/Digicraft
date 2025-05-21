ccefdbb4994e50e60d9ff62a75b16377c1ec4655
goorrooo <gorrgrigoryann@gmail.com>
2025-05-21 21:01:00 +0400

    quiz

--- classwork/loop.cpp
+++ classwork/loop.cpp
@@ -26,7 +26,7 @@ do {
 
 
 
-*/
+
 
 int main(){
 
@@ -37,7 +37,7 @@ std::string log = " ";
 std::string pass = " ";
 std::cout << "Input your login: " << std::endl;
 std::cin >> log;
-/*
+
 std::cout << "Input your password: " << std::endl;
 std::cin >> pass; 
 	if(log != "Student")
@@ -54,7 +54,7 @@ std::cin >> pass;
 }	
 
 
-}*/
+}
 	if(log == "Student"){
 	std::cin >> pass;
 	if(pass == "123"){
@@ -70,6 +70,64 @@ std::cin >> pass;
 return 0;
 
 }
+
+*/
+
+enum eMap { MATHEM, ENGLISH, PROGRAM, UNKNOWN };
+
+
+
+int main( ){
+std::string math = " ";
+std::string engl = " ";
+std::string prog = " ";
+eMap map;
+int i = 0;
+	for(int i = 0; i < 3; i++){
+		eMap map = static_cast<eMap>(i);
+		switch(map){
+			case MATHEM:
+				std::cout << "What is +:" << std::endl;
+				std::cin >> math;
+				if(math == "gumarum"){
+				std::string results = "Your results 1/3 ";}
+				
+			case ENGLISH:
+				std::cout << "What is Englis" << std::endl;
+				std::cin >> engl;
+				if(engl == "lezu"){
+				std::string results = "Your results 2/3 ";}
+			case PROGRAM:
+				std::cout << "HTML, CSS, Java...?" << std::endl;
+				std::cin >> prog;
+				if(prog == "script"){
+				std::string results = "Your results 3/3 ";
+				std::cout << results << std::endl;}
+				
+			default:
+				std::cout << "Error" << std::endl;
+
+
+
+
+
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
