c6ef9089c37846a3d4a21f0beb4fe2dad34155cf
goorrooo <gorrgrigoryann@gmail.com>
2025-06-25 21:04:39 +0400

    inh

--- classwork/class.cpp
+++ classwork/class.cpp
@@ -10,7 +10,6 @@ Car::Car(){
 
 // ~Car(); - destructor 
 
-*/ 
 
 
 class Student{
@@ -86,8 +85,135 @@ int main(){
 
 };
 
+*/
+
+class Person{
+public:
+	Person();
+	Person(std::string name, int age);
+	~Person();
+public:
+	void setName(std::string name);
+	std::string getName();
+	void setAge(int age);
+	int getAge();
+public:
+	void printInfo(){
+	std::cout << getName() << std::endl;
+	std::cout << getAge() << std::endl;
+
+}	
+protected:
+	std::string m_Pname;
+	int m_Page;
 
 
+};
+
+Person::Person()
+{
+	std::string m_Pname = "";
+	int m_Page = 0;
+	
+	std::cout << "Person Ctor" << std::endl;
+};
+
+Person::Person(std::string name, int age){
+	m_Pname = name;
+	m_Page = age;
+};
+
+Person::~Person(){
+	
+	std::cout << "Person Dtor" << std::endl;
+
+};
+void Person::setName(std::string name){
+	m_Pname = name;
+
+};
+std::string Person::getName(){
+	return m_Pname;
+
+};	
+
+void Person::setAge(int age){
+	m_Page = age;
+
+};
+int Person::getAge(){
+	return m_Page;
+};
+
+class Student : public Person{
+		public:
+		Student();
+		Student(std::string name, int age, int grade);
+		~Student();
+		void setGrade(int grade);	
+		int getGrade();
+		public:
+			void printInfoS(){
+			std::cout << getName() << std::endl;
+			std::cout << getAge() << std::endl;
+			std::cout << getGrade() << std::endl;
+
+		}
+		private:
+		int m_Sgrade;
+
+
+};
+Student::Student(){
+
+	std::cout << "Student Ctor" << std::endl;
+};
+ Student::Student(std::string name, int age, int grade){
+	m_Sgrade = grade;
+
+};
+Student::~Student(){
+	std::cout << "St Dtor" << std::endl;
+
+};
+void Student::setGrade(int grade){
+	m_Sgrade = grade;
+}
+int Student::getGrade(){
+	return m_Sgrade;
+}
+	
+class Teucher : public Person{
+	public:
+		Teucher();
+		Teucher(std::string name, int age, std::string lecture);
+	public:	
+		void setLecture(std::string lecture);
+		std::string getLecture();
+	public:
+	void printInfoT(){
+	
+			std::cout << getName() << std::endl;
+			std::cout << getAge() << std::endl;
+			std::cout << getLecture() << std::endl;
+
+};	
+private:
+	std::string m_Tlecture;	
+};
+
+void Teucher::setLecture(std::string lecture){
+	m_Tlecture = lecture;
+};
+std::string Teucher::getLecture(){
+	return m_Tlecture;
+};
+int main(){
+	Person ps("Gor", 12);
+	
+	return 0;
+
+};
 
 
 
