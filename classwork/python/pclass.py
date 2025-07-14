b07b3537a46278191adb6c7466a14dc65ca6c93e
goorrooo <gorrgrigoryann@gmail.com>
2025-07-14 20:38:47 +0400

    polymorphism(python\)

---
+++ classwork/python/pclass.py
@@ -0,0 +1,68 @@
+class Animal:
+   def sound(self):
+    print("Animal sound")
+
+class Dog(Animal):
+   def sound(self):
+    print("Dog sound")
+
+class Cat(Animal):
+   def sounf(self):
+    print("Cat sound")
+
+def makesound(animal: Animal):
+    animal.sound()
+
+makesound(Animal())		
+
+
+class Shape:
+   def area(self):
+    print("Null")
+class Circle:
+   def area(self):
+    print("Circle area")
+class Square:
+   def area(self):
+    print("Square area")
+class Rectangle:
+   def area(self):
+    print("Rectangle area")
+
+shape = [Shape(),Circle(),Square(),Rectangle()]
+
+for i in shape:
+    i.area()
+
+
+
+def add(self,x,y):
+ a = x + y
+  if(a == (str)):
+    print("ss")
+  else: 
+    print("sds")
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
