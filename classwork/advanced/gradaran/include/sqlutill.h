4566a87eb572f82beac592247f85492b47db9f5a
goorrooo <gorrgrigoryann@gmail.com>
2025-10-08 20:24:31 +0400

    sql

--- classwork/advanced/gradaran/include/sqlutill.h
+++ classwork/advanced/gradaran/include/sqlutill.h
@@ -2,10 +2,10 @@
 #define _SQLITE_UTILS_
 #include <sqlite3.h>
 // single ton - optimal version
-bool create(const std::string* file, sqlite3*& db);
+bool create(const std::string& file, sqlite3*& db);
 bool createTable(sqlite*& db);
-bool insertTable(sqlite*& db, const std::string& name, const int& age);
-bool selectTable(sqlite*& db);
+bool insertTable(sqlite3*& db, const std::string& name, const int& age);
+bool selectTable(sqlite3*& db);
 void close(sqlite*& db);
 #endif //!_SQLITE_UTILS
 
